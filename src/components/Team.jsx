efc92ef2d3474d43fbad081b8952a1cf48207e94
Yago <ygarcia@iMac-de-Admin.local>
2025-01-17 10:29:14 +0100

    Estilos aplicados

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,9 +6,19 @@ const Team = ({ team }) => {
    
   return (
     <>
-        <div>
-            <p>{team.team}</p>
+        <div className='conTeam'> 
+          <div className='contRank'>
+            <h1>{team.rank}</h1>
+          </div>
+            
             <img src={team.logo} alt="" />
+            <h2>{team.team}</h2>
+
+            <div className='contDatos'> 
+              <h4>LAST MATCH</h4>
+              <h5>{team.last_played}</h5>
+              <h5>{team.team} {team.last_played_team}</h5>
+            </div>
         </div>
     </>
   )
